c54de9a2dbe233c93100255c57eabe1058f9cab2
Diegothic <diegothic24@gmail.com>
2025-02-07 20:12:44 +0100

    Player Controller

---
+++ Source/Aura/Player/AuraPlayerController.cpp
@@ -0,0 +1,69 @@
+﻿// Copyright Diegothic
+
+
+#include "AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/Character.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(InputMappingContext);
+
+	if (const ULocalPlayer* const LocalPlayer = GetLocalPlayer())
+	{
+		if (
+			UEnhancedInputLocalPlayerSubsystem* const EnhancedInputSubsystem
+				= ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer)
+		)
+		{
+			EnhancedInputSubsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* const EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(
+		MoveInputAction,
+		ETriggerEvent::Triggered,
+		this,
+		&AAuraPlayerController::OnMoveTriggered
+	);
+}
+
+void AAuraPlayerController::OnMoveTriggered(const FInputActionValue& Value)
+{
+	const FVector2D InputAxisVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation(0.0, GetControlRotation().Yaw, 0.0);
+	const FRotationMatrix RotationMatrix(Rotation);
+	const FVector ForwardVector = RotationMatrix.GetUnitAxis(EAxis::X);
+	const FVector RightVector = RotationMatrix.GetUnitAxis(EAxis::Y);
+
+	if (APawn* const ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardVector, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightVector, InputAxisVector.X);
+	}
+}
---
+++ Source/Aura/Player/AuraPlayerController.h
@@ -0,0 +1,31 @@
+﻿// Copyright Diegothic
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+
+private:
+	void OnMoveTriggered(const struct FInputActionValue& Value);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura|Input", Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Aura|Input", Meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UInputAction> MoveInputAction;
+};
