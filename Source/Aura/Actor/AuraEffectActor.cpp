20045e1acc2d3ee888441f329240620459382633
Diegothic <diegothic24@gmail.com>
2025-02-10 14:06:43 +0100

    Test Attribute Modify Actor

---
+++ Source/Aura/Actor/AuraEffectActor.cpp
@@ -0,0 +1,67 @@
+﻿// Copyright Diegothic
+
+
+#include "AuraEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+AAuraEffectActor::AAuraEffectActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	TriggerSphere = CreateDefaultSubobject<USphereComponent>(TEXT("TriggerSphere"));
+	SetRootComponent(TriggerSphere);
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(TriggerSphere);
+}
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TriggerSphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnTriggerBeginOverlap);
+	TriggerSphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::OnTriggerEndOverlap);
+}
+
+void AAuraEffectActor::OnTriggerBeginOverlap_Implementation(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex,
+	bool bFromSweep,
+	const FHitResult& SweepResult
+)
+{
+	// TODO: Change this to apply a Gameplay Effect.
+	// For now using a const_cast hack to modify the attributes.
+	if (OtherActor->Implements<UAbilitySystemInterface>())
+	{
+		if (const IAbilitySystemInterface* const AbilitySystemInterface = Cast<IAbilitySystemInterface>(OtherActor))
+		{
+			if (
+				UAuraAttributeSet* const AttributeSet = const_cast<UAuraAttributeSet*>(Cast<UAuraAttributeSet>(
+					AbilitySystemInterface->GetAbilitySystemComponent()->GetAttributeSet(
+						UAuraAttributeSet::StaticClass())
+				))
+			)
+			{
+				AttributeSet->SetHealth(AttributeSet->GetHealth() + 25.0f);
+
+				Destroy();
+			}
+		}
+	}
+}
+
+void AAuraEffectActor::OnTriggerEndOverlap_Implementation(
+	UPrimitiveComponent* OverlappedComponent,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex
+)
+{
+}
---
+++ Source/Aura/Actor/AuraEffectActor.h
@@ -0,0 +1,44 @@
+﻿// Copyright Diegothic
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraEffectActor.generated.h"
+
+UCLASS()
+class AURA_API AAuraEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAuraEffectActor();
+
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Aura|Effect")
+	void OnTriggerBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Aura|Effect")
+	void OnTriggerEndOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex
+	);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<class USphereComponent> TriggerSphere;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Mesh;
+};
