656fc8dfd579f2b7b65b28c39d7b4ce6e879cdb0
Diegothic <diegothic24@gmail.com>
2025-02-13 15:09:32 +0100

    Clamp Attributes

--- Source/Aura/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/AbilitySystem/AuraAttributeSet.cpp
@@ -3,7 +3,10 @@
 
 #include "AuraAttributeSet.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Net/UnrealNetwork.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
 
 
 UAuraAttributeSet::UAuraAttributeSet()
@@ -25,22 +28,93 @@ void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& Ou
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
 }
 
-void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue)
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxHealth());
+	}
+
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		NewValue = FMath::Max(NewValue, 0.0f);
+	}
+
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, GetMaxMana());
+	}
+
+	if (Attribute == GetMaxManaAttribute())
+	{
+		NewValue = FMath::Max(NewValue, 0.0f);
+	}
+}
+
+void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FEffectProperties EffectProps;
+	FillEffectProperties(Data, EffectProps);
+}
+
+void UAuraAttributeSet::FillEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& OutProps)
+{
+	OutProps.EffectContextHandle = Data.EffectSpec.GetContext();
+	if ((OutProps.InstigatorASC = OutProps.EffectContextHandle.GetInstigatorAbilitySystemComponent()))
+	{
+		if (
+			OutProps.InstigatorASC->AbilityActorInfo.IsValid()
+			&& OutProps.InstigatorASC->AbilityActorInfo->AvatarActor.IsValid()
+		)
+		{
+			OutProps.InstigatorAvatarActor = OutProps.InstigatorASC->GetAvatarActor();
+
+			AController* InstigatorController = OutProps.InstigatorASC->AbilityActorInfo->PlayerController.Get();
+			if (!InstigatorController && OutProps.InstigatorAvatarActor)
+			{
+				if (const APawn* const InstigatorPawn = Cast<APawn>(OutProps.InstigatorAvatarActor))
+				{
+					InstigatorController = InstigatorPawn->GetController();
+				}
+			}
+			OutProps.InstigatorController = InstigatorController;
+
+			if (InstigatorController)
+			{
+				OutProps.InstigatorCharacter = Cast<ACharacter>(InstigatorController->GetPawn());
+			}
+		}
+	}
+
+	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+	{
+		OutProps.TargetAvatarActor = Data.Target.GetAvatarActor();
+		OutProps.TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		OutProps.TargetCharacter = Cast<ACharacter>(OutProps.TargetAvatarActor);
+		OutProps.TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OutProps.TargetAvatarActor);
+	}
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldValue) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldValue);
 }
 
-void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue)
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldValue);
 }
 
-void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue)
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldValue) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldValue);
 }
 
-void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue)
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldValue) const
 {
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldValue);
 }
--- Source/Aura/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/AbilitySystem/AuraAttributeSet.h
@@ -14,6 +14,41 @@
 	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
+USTRUCT()
+struct FEffectProperties
+{
+	GENERATED_BODY()
+
+	FEffectProperties();
+
+	UPROPERTY()
+	FGameplayEffectContextHandle EffectContextHandle;
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> InstigatorASC = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AActor> InstigatorAvatarActor = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AController> InstigatorController = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<ACharacter> InstigatorCharacter = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> TargetASC = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AActor> TargetAvatarActor = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AController> TargetController = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<ACharacter> TargetCharacter = nullptr;
+};
+
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
@@ -30,16 +65,24 @@ public:
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
 
+	//~ Begin UAttributeSet Interface
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
+	//~ End UAttributeSet Interface
+
 protected:
 	UFUNCTION()
-	void OnRep_Health(const FGameplayAttributeData& OldValue);
+	void OnRep_Health(const FGameplayAttributeData& OldValue) const;
 	UFUNCTION()
-	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue);
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const;
 
 	UFUNCTION()
-	void OnRep_Mana(const FGameplayAttributeData& OldValue);
+	void OnRep_Mana(const FGameplayAttributeData& OldValue) const;
 	UFUNCTION()
-	void OnRep_MaxMana(const FGameplayAttributeData& OldValue);
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue) const;
+
+private:
+	static void FillEffectProperties(const FGameplayEffectModCallbackData& Data, FEffectProperties& OutProps);
 
 private:
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Aura|Attributes|Health",
