b0a0a51d4483e3d26cb7c0e203ecfcc463380353
Diegothic <diegothic24@gmail.com>
2025-03-02 17:13:11 +0100

    Attribute Menu Widget Controller Class

---
+++ Source/Aura/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright Diegothic
+
+
+#include "AttributeMenuWidgetController.h"
+
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+}
---
+++ Source/Aura/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,20 @@
+﻿// Copyright Diegothic
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraWidgetController.h"
+
+#include "AttributeMenuWidgetController.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+
+public:
+	//~ Begin UAuraWidgetController Interface
+	virtual void BroadcastInitialValues() override;
+	virtual void BindCallbacksToDependencies() override;
+	//~ End UAuraWidgetController Interface
+};
