985f503bfd79a1c1c8e29e46179b089ecb58673e
Diegothic <diegothic24@gmail.com>
2025-04-22 08:13:27 +0100

    Projectile Class

---
+++ Source/Aura/Actor/AuraProjectile.h
@@ -0,0 +1,48 @@
+﻿// Copyright Diegothic
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AuraProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class AURA_API AAuraProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAuraProjectile();
+
+protected:
+	//~ Begin AActor Interface
+	virtual void BeginPlay() override;
+	//~ End AActor Interface
+
+public:
+	FORCEINLINE const UProjectileMovementComponent* GetProjectileMovement() const
+	{
+		return ProjectileMovement;
+	}
+
+private:
+	UFUNCTION()
+	void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Aura|Projectile")
+	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Aura|Projectile", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+};
