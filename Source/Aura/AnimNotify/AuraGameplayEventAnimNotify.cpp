f388fe48e2d581e4e5c4fb34ea9e11767845cdde
Diegothic <diegothic24@gmail.com>
2025-04-29 16:50:25 +0200

    Gameplay Event Anim Notify

---
+++ Source/Aura/AnimNotify/AuraGameplayEventAnimNotify.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Diegothic
+
+
+#include "AuraGameplayEventAnimNotify.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+
+
+void UAuraGameplayEventAnimNotify::Notify(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference
+)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	AActor* const OwnerActor = MeshComp->GetOwner();
+	const FGameplayEventData EventData;
+	UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(OwnerActor, EventTag, EventData);
+}
---
+++ Source/Aura/AnimNotify/AuraGameplayEventAnimNotify.h
@@ -0,0 +1,25 @@
+﻿// Copyright Diegothic
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AuraGameplayEventAnimNotify.generated.h"
+
+UCLASS()
+class AURA_API UAuraGameplayEventAnimNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference
+	) override;
+
+private:
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Aura|Event", meta = (AllowPrivateAccess = "true"))
+	FGameplayTag EventTag;
+};
